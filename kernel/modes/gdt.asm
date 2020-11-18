00f2d4fcbf0324cad7991d90c0006b5f4da43305
EldarionDev <janek.schultes.privat@gmail.com>
2020-11-18 17:05:25 +0100

    Created GDT

--- kernel/modes/gdt.asm
+++ kernel/modes/gdt.asm
@@ -1,4 +1,35 @@
 global load_gdt
 
+gdt_start:
+
+gdt_null:
+	dd 0x0
+	dd 0x0
+
+gdt_code:
+	dw 0xffff
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+
+gdt_data:
+	dw 0xffff
+	dw 0x0
+	db 0x0
+	db 10010010b
+	db 11001111b
+	db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start - 1
+	dd gdt_start
+
+%define CODE_SEGMENT gdt_code - gdt_start
+%define DATA_SEGMENT gdt_data - gdt_start
+
 load_gdt:
 	ret	
